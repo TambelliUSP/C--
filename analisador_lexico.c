5265a85871bc28a7b65751560907d1b36ec14a3b
TambelliUSP <gabrieldtambelli@usp.br>
2024-07-23 14:56:11 -0300

    analisador lexico básico

---
+++ analisador_lexico.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef enum {
+    VR, NM, IT, SC, PT, IF, PV, AT, AP, FP, OP, CP, EF, UNKNOWN
+} TokenType;
+
+const char* tokenNames[] = {
+    "VR", "NM", "IT", "SC", "PT", "IF", "PV", "AT", "AP", "FP", "OP", "CP", "EF", "UNKNOWN"
+};
+
+typedef struct {
+    TokenType type;
+    char word[100];
+} Token;
+
+void printToken(Token token) {
+    printf("Token: %s, word: %s\n", tokenNames[token.type], token.word);
+}
+
+bool isKeyword(char* str) {
+    return (strcmp(str, "int") == 0 || strcmp(str, "scanf") == 0 || strcmp(str, "printf") == 0 || strcmp(str, "if") == 0);
+}
+
+TokenType getKeywordTokenType(char* str) {
+    if (strcmp(str, "int") == 0) return IT;
+    if (strcmp(str, "scanf") == 0) return SC;
+    if (strcmp(str, "printf") == 0) return PT;
+    if (strcmp(str, "if") == 0) return IF;
+    return UNKNOWN;
+}
+
+TokenType getCharTokenType(char c) {
+    switch (c) {
+        case ';': return PV;
+        case '=': return AT;
+        case '(': return AP;
+        case ')': return FP;
+        case '+': return OP;
+        case '-': return OP;
+        case '*': return OP;
+        case '/': return OP;
+        default: return UNKNOWN;
+    }
+}
+
+TokenType getCompOperatorTokenType(char* str) {
+    if (strcmp(str, "==") == 0 || strcmp(str, "<") == 0 || strcmp(str, ">") == 0 || strcmp(str, "<=") == 0 || strcmp(str, ">=") == 0) return CP;
+    return UNKNOWN;
+}
+
+Token getNextToken(FILE* source) {
+    Token token;
+    token.type = UNKNOWN;
+    int index = 0;
+    char ch;
+
+    while ((ch = fgetc(source)) != EOF) {
+        if (isspace(ch)) {
+            continue;
+        } else if (isalpha(ch) || ch == '_') {
+            token.word[index++] = ch;
+            while (isalnum(ch = fgetc(source)) || ch == '_') {
+                token.word[index++] = ch;
+            }
+            ungetc(ch, source);
+            token.word[index] = '\0';
+
+            if (isKeyword(token.word)) {
+                token.type = getKeywordTokenType(token.word);
+            } else {
+                token.type = VR;
+            }
+            return token;
+
+        } else if (isdigit(ch)) {
+            token.word[index++] = ch;
+            while (isdigit(ch = fgetc(source))) {
+                token.word[index++] = ch;
+            }
+            ungetc(ch, source);
+            token.word[index] = '\0';
+            token.type = NM;
+            return token;
+
+        } else if (ch == '=' || ch == '<' || ch == '>') {
+            token.word[index++] = ch;
+            if ((ch = fgetc(source)) == '=') {
+                token.word[index++] = ch;
+            } else {
+                ungetc(ch, source);
+            }
+            token.word[index] = '\0';
+            token.type = getCompOperatorTokenType(token.word);
+            return token;
+
+        } else {
+            token.word[index++] = ch;
+            token.word[index] = '\0';
+            token.type = getCharTokenType(ch);
+            return token;
+        }
+    }
+
+    token.type = EF;
+    strcpy(token.word, "EOF");
+    return token;
+}
+
+int main() {
+    FILE* source = fopen("input.c", "r");
+    Token token;
+
+    do {
+        token = getNextToken(source);
+        printToken(token);
+    } while (token.type != EF);
+
+    fclose(source);
+    return EXIT_SUCCESS;
+}
